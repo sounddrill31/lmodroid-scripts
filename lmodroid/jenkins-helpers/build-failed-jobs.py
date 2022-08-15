bb5e8b074c11bbbd53292e3efff80ccf69a89643
Erfan Abdi <erfangplus@gmail.com>
2022-08-15 15:35:58 +0430

    Add jenkins red builder

---
+++ lmodroid/jenkins-helpers/build-failed-jobs.py
@@ -0,0 +1,21 @@
+import os
+from jenkins import Jenkins
+
+JENKINS_USERNAME = os.environ.get("JENKINS_USERNAME")
+JENKINS_PASSWORD = os.environ.get("JENKINS_PASSWORD")
+
+server = Jenkins('https://jenkins.libremobileos.com', 
+                 username=JENKINS_USERNAME, password=JENKINS_PASSWORD)
+user = server.get_whoami()
+version = server.get_version()
+print('%s Connected to Jenkins %s' % (user['fullName'], version))
+
+jobs = server.get_jobs()
+for job in jobs:
+    if job["name"] == "LMODroid":
+        jobs = job["jobs"]
+        break
+
+for job in jobs:
+    if job["color"] == "red":
+        server.build_job("LMODroid/" + job["name"])
