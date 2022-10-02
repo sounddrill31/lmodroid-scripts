aecf6401d20415ee4dc12ccb4a25c561e30f1ce2
Erfan Abdi <erfangplus@gmail.com>
2022-10-02 08:36:38 +0330

    add more jenkins scripts
    * Make them all into one?

---
+++ lmodroid/jenkins-helpers/cancel-queue-jobs.py
@@ -0,0 +1,15 @@
+import os
+from jenkins import Jenkins
+
+JENKINS_USERNAME = os.environ.get("JENKINS_USERNAME")
+JENKINS_PASSWORD = os.environ.get("JENKINS_PASSWORD")
+
+server = Jenkins('https://jenkins.libremobileos.com', 
+                 username=JENKINS_USERNAME, password=JENKINS_PASSWORD)
+user = server.get_whoami()
+version = server.get_version()
+print('%s Connected to Jenkins %s' % (user['fullName'], version))
+
+jobs = server.get_queue_info()
+for job in jobs:
+    server.cancel_queue(job['id'])
