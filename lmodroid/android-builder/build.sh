f7d95050d89e1d4d5500d66e6529f221964d8f8e
Erfan Abdi <erfangplus@gmail.com>
2024-04-25 21:09:51 +0330

    Revert "Add ap1a for fourteen"
    This reverts commit cbaf6cd8a493da8f02b1bb4dcc15cd82557a21a8.

--- lmodroid/android-builder/build.sh
+++ lmodroid/android-builder/build.sh
@@ -7,7 +7,6 @@ echo DEVICE=$DEVICE
 echo ROM_VERSION=$ROM_VERSION
 echo BUILD_TYPE=$BUILD_TYPE
 echo LMODROID_BUILDTYPE=$LMODROID_BUILDTYPE
-echo RELEASE_TYPE=$RELEASE_TYPE
 echo CCACHE_DIR=$CCACHE_DIR
 echo ROOTDIR=$ROOTDIR
 echo WORKSPACE=$WORKSPACE
@@ -18,7 +17,7 @@ echo '[+] Setup Environment...'
 . build/envsetup.sh
 
 echo '[+] Breakfast...'
-breakfast lmodroid_${DEVICE}${RELEASE_TYPE}-${BUILD_TYPE}
+breakfast lmodroid_${DEVICE}-${BUILD_TYPE}
 
 echo '[+] Cleaning...'
 rm -rf $ROOTDIR/out
@@ -29,6 +28,6 @@ make bacon -j$(nproc --all)
 if [[ ! $? -eq 0 ]]; then
     echo '[+] Cleaning...'
     rm -rf $ROOTDIR/*
-
+    
     exit 1
 fi
